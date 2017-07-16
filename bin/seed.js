88689482a7997bff0aa5577941aacd82e26de0a8
Mikael Dunhem <mikael.dunhem@gmail.com>
2017-07-16 18:28:38 -0400

    Update vehicle model

--- bin/seed.js
+++ bin/seed.js
@@ -14,16 +14,16 @@ mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
 mongoose.connection.on("open", () => {
   mongoose.connection.db.dropDatabase(error => {
     if (error) {
-      console.error(error);
-      process.exit();
+      throw error;
     }
 
     const vehicle = new Vehicle({ vin: "FE1", year: 2003, make: "Toyota", vehicleModel: "Corolla" });
     vehicle.save(saveError => {
       if (saveError) {
-        console.error(saveError);
+        throw saveError;
       } else {
         console.log("Seeded the db");
+        process.exit();
       }
     });
   });
