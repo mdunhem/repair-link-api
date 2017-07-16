5b8dc5df8f28952b0e7adea0ed1e7a8f0815cbff
Mikael Dunhem <mikael.dunhem@gmail.com>
2017-07-16 18:04:32 -0400

    Update seed file to proper node syntax

--- bin/seed.js
+++ bin/seed.js
@@ -1,5 +1,13 @@
-import * as mongoose from "mongoose";
-import { Vehicle } from "./models/vehicle";
+const mongoose = require("mongoose");
+
+const VehicleSchema = new mongoose.Schema({
+  vin: { type: Number, unique: true },
+  year: Number,
+  make: String,
+  model: String
+});
+
+const Vehicle = mongoose.model("Vehicle", VehicleSchema);
 
 mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
 
