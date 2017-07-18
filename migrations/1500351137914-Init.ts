3ff2e13165e99d3e733790a702a9bdd9f789e5de
Mikael Dunhem <mikael.dunhem@gmail.com>
2017-07-18 18:04:15 -0400

    Add config and migration file for initial db migration

---
+++ migrations/1500351137914-Init.ts
@@ -0,0 +1,22 @@
+import {Connection, EntityManager, MigrationInterface, QueryRunner} from "typeorm";
+
+export class Init1500351137914 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(
+            `CREATE TABLE IF NOT EXISTS vehicle (
+                id serial PRIMARY KEY,
+                vin VARCHAR (255) UNIQUE NOT NULL,
+                year integer NOT NULL,
+                make VARCHAR (255) NOT NULL,
+                model VARCHAR (255) NOT NULL,
+                created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT clock_timestamp()
+            );`
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.dropTable("vehicle");
+    }
+
+}
