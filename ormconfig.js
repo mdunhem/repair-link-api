fd139604cd70c18d56abbec4ad4d0798f3e196bf
Mikael Dunhem <mikael.dunhem@gmail.com>
2017-07-19 00:26:56 -0400

    Remove unnecessary quotes in ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,53 +1,53 @@
 module.exports = [
   {
-    "name": "localhost",
-    "driver": {
-      "type": "postgres",
-      "host": "localhost",
-      "port": 5432,
-      "username": "repairlink",
-      "password": "password",
-      "database": "repairlink"
+    name: "localhost",
+    driver: {
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "repairlink",
+      password: "password",
+      database: "repairlink"
     },
-    "autoSchemaSync": true,
-    "entities": [
+    autoSchemaSync: true,
+    entities: [
       "dist/entity/*.js"
     ],
-    "subscribers": [
+    subscribers: [
       "dist/subscriber/*.js,"
     ],
-    "migrations": [
+    migrations: [
       "dist/migrations/*.js"
     ],
-    "cli": {
-      "entitiesDir": "src/entity",
-      "migrationsDir": "src/migrations",
-      "subscribersDir": "src/subscriber"
+    cli: {
+      entitiesDir: "src/entity",
+      migrationsDir: "src/migrations",
+      subscribersDir: "src/subscriber"
     }
   },
   {
-    "name": "prod",
-    "driver": {
-      "type": "postgres",
-      "url": process.env.DATABASE_URL,
+    name: "prod",
+    driver: {
+      type: "postgres",
+      url: process.env.DATABASE_URL,
       extra: {
         ssl: true
       }
     },
-    "autoSchemaSync": true,
-    "entities": [
+    autoSchemaSync: true,
+    entities: [
       "dist/entity/*.js"
     ],
-    "subscribers": [
+    subscribers: [
       "dist/subscriber/*.js,"
     ],
-    "migrations": [
+    migrations: [
       "dist/migrations/*.js"
     ],
-    "cli": {
-      "entitiesDir": "src/entity",
-      "migrationsDir": "src/migrations",
-      "subscribersDir": "src/subscriber"
+    cli: {
+      entitiesDir: "src/entity",
+      migrationsDir: "src/migrations",
+      subscribersDir: "src/subscriber"
     }
   }
 ];
